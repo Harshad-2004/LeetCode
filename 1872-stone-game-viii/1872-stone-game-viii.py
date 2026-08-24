3070bcf80137326db2b3b09a54cfeedddc28fba2
Harshad-2004 <harshadsawant1207@gmail.com>
2026-08-24 22:47:15 +0530

    Time: 855 ms (18.18%), Space: 24.3 MB (50%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def stoneGameVIII(self, stones):
+        n = len(stones)
+        
+        # Calculate prefix sum
+        prefix = [0] * n
+        prefix[0] = stones[0]
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] + stones[i]
+            
+        # Backward DP
+        dp = prefix[-1]
+        for i in range(n - 2, 0, -1):
+            dp = max(dp, prefix[i] - dp)
+            
+        return dp
